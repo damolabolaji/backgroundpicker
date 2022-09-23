f8ef79eafd804f8c9383be073dd26626d6334840
Bolaji Oyindamola <86451863+damolabolaji@users.noreply.github.com>
2022-09-23 12:09:21 +0100

    Update main.js
    added functions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
 
+document.getElementById('darkcyan').onclick = colorDarkCyan
+document.getElementById('beige').onclick = colorBeige
+document.getElementById('blue').onclick = colorBlue
+document.getElementById('gray').onclick = colorGray
+document.getElementById('mustard').onclick = colorMustard
+  
+function colorDarkCyan(){
+  document.querySelector('body').style.backgroundColor = #3d7e84
+  document.querySelector('body').style.color = white
+}
+
+function colorBeige(){
+  document.querySelector('body').style,backgroundColor = #d3d2b7
+  document.querySelector('body').style.color = white
+}
+
+function colorBlue(){
+  document.querySelector('body').style,backgroundColor =  #8b9ce4
+  document.querySelector('body').style.color = white
+}
+
+function colorGray(){
+  document.querySelector('body').style,backgroundColor = lightgray
+  document.querySelector('body').style.color = white
+}
+
+function colorMustard(){
+  document.querySelector('body').style,backgroundColor = #f4d559
+  document.querySelector('body').style.color = white
+}
+
+
+
