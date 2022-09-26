c34452fcf2b040178c2c621610aaf6955fdb4c27
Bolaji Oyindamola <86451863+damolabolaji@users.noreply.github.com>
2022-09-26 23:48:02 +0100

    Update main.js
    added EventListeners for the list items

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
+document.querySelector('#darkcyan').addEventListener('click', darkCyan)
+document.querySelector('#beige').addEventListener('click', beige)
+document.querySelector('#blue').addEventListener('click', blue)
+document.querySelector('#gray').addEventListener('click', gray)
+document.querySelector('#mustard').addEventListener('click', mustard)
 
