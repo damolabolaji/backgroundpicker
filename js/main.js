b7f14af229a6abb9e657a39c0ef403846d75089a
Bolaji Oyindamola <86451863+damolabolaji@users.noreply.github.com>
2022-09-26 23:54:34 +0100

    Update main.js
    adding functions

--- js/main.js
+++ js/main.js
@@ -4,3 +4,12 @@ document.querySelector('#blue').addEventListener('click', blue)
 document.querySelector('#gray').addEventListener('click', gray)
 document.querySelector('#mustard').addEventListener('click', mustard)
 
+function darkCyan(){
+  document.querySelector('body').style.backgroundColor = '#3d7e84'
+  document.querySelector('body').style. color = 'white'
+}
+
+function beige(){
+  document.querySelector('body').style.backgroundColor = '#d3d2b7'
+  document.querySelector('body').style. color = 'black'
+}
